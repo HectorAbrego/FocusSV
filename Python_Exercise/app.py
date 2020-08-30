5fdfe6611ca9888527c124356a9feb10d6acd9d0
Héctor José Abrego <70445306+HectorAbrego@users.noreply.github.com>
2020-08-30 15:41:19 -0600

    Add files via upload
    The app.py is updated. It's added code to create a new file with the top 20 podcasts

--- Python_Exercise/app.py
+++ Python_Exercise/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 from podcasts import podcasts
+import json
 
 app = Flask(__name__)
 
+#The 3 next routes are focused On reading and filtering the JSON
 # If you want to see the all podcasts
 @app.route('/podcasts') 
 def getPodcasts():
@@ -23,6 +25,33 @@ def getPodcastName(podcast_artistName):
     if (len(podcastFound) > 0):
         return jsonify({"podcast": podcastFound})
     return jsonify({"message":"Podcast not found"})
+
+
+#The next route will help us to create a file with the top 20 podcasts
+@app.route('/podcaststop20') 
+def getPodcastsTop20():
+    podcaststop20 = [podcast for podcast in  podcasts] #with this line all the JSON is read
+    pod = 0
+    data = ''
+
+    if (len(podcaststop20) > 0): # The JSON is validated to have information
+        while pod <= 19: # the top 20 podcasts are read and saved in the variable data
+            data = data + str(podcaststop20[pod])
+            pod = pod + 1
     
+    with open('podcastTop20.json', 'w') as json_file: # The new JSON with the top 20 is created
+        json.dump(data, json_file, indent=4)
+    
+    return jsonify({"message":"Top 20 Podcasts were created successfully in podcastTop20.json. Please, verify on your main folder"})
+
+    
+   
+       # return jsonify({"podcast": podcaststop20})
+    
+    # podcaststop20 = jsonify(podcasts)
+    # with open('podcastsTop20.json', 'w') as json_file:
+    #     json.dump(podcaststop20, json_file)
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
